4cbfda5590929c2607413a9c7c09fffbaff4bc4e
Tate Shepherd <tshepherd@kollecitve.com>
2020-10-19 14:07:06 -0700

    added a github link n stuff

--- plopfile.js
+++ plopfile.js
@@ -4,8 +4,8 @@ module.exports = function (plop) {
       description: 'This is a component plop',
       prompts: [{
         type: 'input',
-        name: 'name',
-        message: 'What is the name of the component we are making?'
+        name: '{{name}}',
+        message: 'What will be the name of the component?'
       },
     ], // array of inquirer prompts
       actions: [{
--- src/Components/TateIntro/TateIntro.js
+++ src/Components/TateIntro/TateIntro.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Container, Icon } from "nes-react";
-
+import { Container, Icon, Sprite } from "nes-react";
+import styles from './TateIntro.module.css';
 /** actions */
 
 /** helpers */
@@ -8,11 +8,19 @@ import { Container, Icon } from "nes-react";
 
 export const TateIntro = () => {
   const stars = ['','','','',''].map((item, value) => (<Icon icon="star" key={value} className='star-icon' large />));
+
   return (
-    <section>
+    <section className={styles.contentVertical}>
      <h1>Tate Shepherd</h1>
      <Container title="Five Star Website">
        {stars}
+     </Container>
+     <Container >
+      <h2>Find me on Github</h2>
+      <div className={styles.contentCentered}>
+        <Sprite sprite='octocat' />
+        <a className={styles.githubLink} href='https://github.com/tateshep' target='_blank'>@tateshep</a>
+      </div>
      </Container>
     </section>
   );
