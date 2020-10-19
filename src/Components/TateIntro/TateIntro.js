2e629052f4885d24a05b5229496db215bbfee421
Tate Shepherd <tshepherd@kollecitve.com>
2020-10-19 15:33:11 -0700

    more style edits, added intro paragraph

--- src/Components/TateIntro/TateIntro.js
+++ src/Components/TateIntro/TateIntro.js
@@ -12,12 +12,14 @@ export const TateIntro = () => {
   return (
     <section>
      <h1>Tate Shepherd</h1>
-     <Container title="Five Star Website">
+     <Container className={styles.contentContainer} title="Five Star Website">
+       <p>Hello,</p>
+       <p>This is a fantastic website is it not? I would absolutely give it five stars. If you think it is not worth all those stars, well take it up with me directly. I won't have you chicken-hearted laggards talking ill about my website behind my back. If you do think it is as great as I do, then I'd like to say to you from the bottom of my heart, I feel you are a superb judge of websites. Please reach out to me via one of the contact methods listed on this page.</p>
        <div className={styles.starsContainer}>
          {stars}
        </div>
      </Container>
-     <Container >
+     <Container className={styles.contentContainer}>
       <h2>Find me on Github</h2>
       <div className={styles.contentCentered}>
         <Sprite sprite='octocat' />
