0e2640204a9407af9cd0627253dcadc1d2146e76
Tate Shepherd <tshepherd@kollecitve.com>
2020-10-20 09:17:35 -0700

    text edits

--- src/Components/TateIntro/TateIntro.js
+++ src/Components/TateIntro/TateIntro.js
@@ -14,7 +14,7 @@ export const TateIntro = () => {
      <h1>Tate Shepherd</h1>
      <Container className={styles.contentContainer} title="Five Star Website">
        <p>Hello,</p>
-       <p>This is a fantastic website is it not? I would absolutely give it five stars. If you think it is not worth all those stars, well take it up with me directly. I won't have you chicken-hearted laggards talking ill about my website behind my back. If you think it <em>is</em> as great as I do, then I'd like to say to you from the bottom of my heart, I feel you are a superb judge of websites. Please reach out to me via one of the contact methods listed on this page.</p>
+       <p>This is a fantastic website is it not? I would absolutely give it five stars. If you think it is not worth all those stars take it up with me directly. I won't have you chicken-hearted laggards talking ill about my website behind my back. If you think it <em>is</em> as great as I do, then I'd like to say to you from the bottom of my heart, I feel you are a superb judge of websites. Please reach out to me via one of the contact methods listed on this page.</p>
        <div className={styles.starsContainer}>
          {stars}
        </div>
